c563bd3069f8b8a3ce1c17b692a2e32fa343d174
World B Free <44327036+Adennors@users.noreply.github.com>
2018-12-13 23:37:48 -0500

    final edition

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,3 +1,34 @@
-print("This Python stuff is really hard, yo")
+import os
+
+import csv
+
+BudgetDataCSV = os.path.join("..", "Resources", "budget_data.csv")
+
+# Create lists to store data
+TotalMonths = []
+NetProfit = []
+AverageMonthlyProfitChange = []
+LargestProfitIncrease = []
+LargestLossDecrease = []
+
+# Open and read the csv file
+with open(BudgetDataCSV, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    next(csvreader, None)
+    for row in csvreader:
+        TotalMonths.append(row[1])
+        NetProfit.append(row[2])
+        AverageMonthlyProfitChange.append(row[3])
+        LargestProfitIncrease.append(row[4])
+        LargestLossDecrease.append(row[5])
+        
 
 
+
+
+
+    
+
+
+
+# Convert integer months into string and print
